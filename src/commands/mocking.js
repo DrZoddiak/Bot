da82fa702eda05b98b1b1361a387d944584dad48
Jesse McKee <jessemckee97@gmail.com>
2022-01-13 22:55:09 -0600

    mocking.js Mocks a users message

---
+++ src/commands/mocking.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('mock')
+		.setDescription('Mocks the message!')
+		.addStringOption(option =>
+			option.setName('message')
+				.setDescription('The message you want to mock')
+				.setRequired(true)),
+	async execute(interaction) {
+		const msg = interaction.options.getString('input');
+
+		const mockMsg = msg.split('').map((v) =>
+			Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase(),
+		).join('');
+
+		return interaction.reply(mockMsg);
+	},
+};
