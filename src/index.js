b32f9f6394e2a38f943cc3ace9a2e477ce1f8fbd
Jesse McKee <DrZoddiak@gmail.com>
2022-01-16 14:41:52 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = process.env.BOT_KEY;
 
 const client = new Client({ intents: [
 	Intents.FLAGS.GUILD_MESSAGES,
